f72b598e5603606ee1fa8218c38443830bcadb4d
chyves <chyvescontact@gmail.com>
2025-07-06 22:45:13 -0400

    micro-optimizations

--- src-client/memory/patches.cpp
+++ src-client/memory/patches.cpp
@@ -2,31 +2,32 @@
 
 #include <future>
 
-#include "src-client/gui/controls/renderers/SplashTextRenderer.hpp"
 #include "patterns/latest_windows.hpp"
 
 namespace selaura {
     void patch_manager::init() {
-        std::async(std::launch::deferred, [&] {
-            REGISTER_HOOK(
-                &bgfx::d3d11::RendererContextD3D11::submit_hk,
-                reinterpret_cast<void**>(&selaura::trampolines::bgfx_d3d11_RendererContextD3D11_submit)
-            );
+        std::vector<std::future<void>> tasks;
 
-            REGISTER_HOOK(
-                &bgfx::d3d12::RendererContextD3D12::submit_hk,
-                reinterpret_cast<void**>(&selaura::trampolines::bgfx_d3d12_RendererContextD3D12_submit)
-            );
+        tasks.push_back(std::async(std::launch::async, [&] {
+            REGISTER_HOOK(&bgfx::d3d11::RendererContextD3D11::submit_hk,
+                          reinterpret_cast<void**>( &selaura::trampolines::bgfx_d3d11_RendererContextD3D11_submit));
+        }));
 
-            REGISTER_HOOK(
-                &ScreenView::setupAndRender_hk,
-                reinterpret_cast<void**>(&selaura::trampolines::ScreenView_setupAndRender)
-            );
+        tasks.push_back(std::async(std::launch::async, [&] {
+            REGISTER_HOOK(&bgfx::d3d12::RendererContextD3D12::submit_hk,
+                          reinterpret_cast<void**>(&selaura::trampolines::bgfx_d3d12_RendererContextD3D12_submit));
+        }));
 
-            REGISTER_HOOK(
-                &GameArguments::_onUri_hk,
-                reinterpret_cast<void**>(&selaura::trampolines::GameArguments_onUri)
-            );
-        }).get();
+        tasks.push_back(std::async(std::launch::async, [&] {
+            REGISTER_HOOK(&ScreenView::setupAndRender_hk,
+                          reinterpret_cast<void**>(&selaura::trampolines::ScreenView_setupAndRender));
+        }));
+
+        tasks.push_back(std::async(std::launch::async, [&] {
+            REGISTER_HOOK(&GameArguments::_onUri_hk,
+                          reinterpret_cast<void**>(&selaura::trampolines::GameArguments_onUri));
+        }));
+
+        for (auto& task : tasks) task.get();
     }
 };
