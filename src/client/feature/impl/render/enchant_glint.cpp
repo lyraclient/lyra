fae7e9090caf40746aa7d55263299ba6e3de9801
notchyves <chyvescontact@gmail.com>
2025-07-18 15:03:10 -0400

    chroma enchantment glint

--- src/client/feature/impl/render/enchant_glint.cpp
+++ src/client/feature/impl/render/enchant_glint.cpp
@@ -12,10 +12,39 @@ namespace selaura {
         evm.unsubscribe(&enchant_glint::on_renderiteminhanddescription_event, this);
     }
 
+    glm::vec4 enchant_glint::get_chroma_color(float speed, float saturation, float value) {
+        using namespace std::chrono;
+        float time = duration_cast<milliseconds>(steady_clock::now().time_since_epoch()).count() / 1000.0f;
+        float hue = fmodf(time * speed * 60.0f, 360.0f);
+
+        float c = value * saturation;
+        float x = c * (1 - fabsf(fmodf(hue / 60.0f, 2) - 1));
+        float m = value - c;
+
+        float r = 0, g = 0, b = 0;
+        if (0 <= hue && hue < 60)      r = c, g = x, b = 0;
+        else if (60 <= hue && hue < 120)  r = x, g = c, b = 0;
+        else if (120 <= hue && hue < 180) r = 0, g = c, b = x;
+        else if (180 <= hue && hue < 240) r = 0, g = x, b = c;
+        else if (240 <= hue && hue < 300) r = x, g = 0, b = c;
+        else if (300 <= hue && hue < 360) r = c, g = 0, b = x;
+
+        return { r + m, g + m, b + m, 1.0f };
+    }
+
     void enchant_glint::on_renderiteminhanddescription_event(selaura::RenderItemInHandDescription_event &event) {
         auto& color = std::get<0>(this->settings).value;
-        event.color = {color.r / 255, color.g / 255, color.b / 255};
-        event.alpha = color.a;
+        auto& chroma = std::get<1>(this->settings).value;
+        auto& chroma_speed = std::get<2>(this->settings).value;
+
+        if (chroma) {
+            auto chroma_col = get_chroma_color(chroma_speed);
+            event.color = { chroma_col.r, chroma_col.g, chroma_col.b };
+            event.alpha = chroma_col.a;
+        } else {
+            event.color = {color.r / 255, color.g / 255, color.b / 255};
+            event.alpha = color.a;
+        }
     }
 
 };
--- src/client/feature/impl/render/enchant_glint.hpp
+++ src/client/feature/impl/render/enchant_glint.hpp
@@ -13,17 +13,22 @@ namespace selaura {
         static constexpr auto description = hat::fixed_string{ "Modifies the color of the enchantment glint on items" };
 
         using settings = std::tuple<
-            selaura::feature_setting<glm::vec4>
+            selaura::feature_setting<glm::vec4>,
+            selaura::feature_setting<bool>,
+            selaura::feature_setting<float>
         >;
     };
 
     struct enchant_glint : feature<enchant_glint> {
         enchant_glint() {
             std::get<0>(this->settings) = selaura::feature_setting<glm::vec4>("Color", {73, 36, 116, 1});
+            std::get<1>(this->settings) = selaura::feature_setting<bool>("Chroma", true);
+            std::get<2>(this->settings) = selaura::feature_setting<float>("Chroma Speeding", 1.0f, 0.0f, 5.0f, 0.1f);
         }
 
         void on_enable() override;
         void on_disable() override;
+        glm::vec4 get_chroma_color(float speed = 1.0f, float saturation = 1.0f, float value = 1.0f);
         void on_renderiteminhanddescription_event(selaura::RenderItemInHandDescription_event& event);
     };
 
